ad9f40c3e0c6dc0cf7ae906731ad8b85336c4323
Carlos <carloscolomer99@gmail.com>
2023-06-16 12:21:36 +0200

    pequeña mejora

--- PEC6_Ej_Prac/stock-market/src/app/services/article-service.service.ts
+++ PEC6_Ej_Prac/stock-market/src/app/services/article-service.service.ts
@@ -17,41 +17,6 @@ export class ArticleService {
 
     this.articulos = []
 
-    // let articulo: Article = new Article();
-
-    // articulo.id = 1;
-    // articulo.name = 'Pizza';
-    // articulo.imageUrl = 'https://images.unsplash.com/photo-1613564834361-9436948817d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1343&q=80';
-    // articulo.price = 23;
-    // articulo.isOnSale = true;
-    // articulo.quantityInCart = 0;
-
-    // this.articulos.push(articulo);
-
-    // articulo = new Article();
-
-    // articulo.id = 2;
-    // articulo.name = 'Pan';
-    // articulo.imageUrl = 'https://images.unsplash.com/photo-1559811814-e2c57b5e69df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
-    // articulo.price = 1.5;
-    // articulo.isOnSale = false;
-    // articulo.quantityInCart = 0;
-
-    // this.articulos.push(articulo);
-
-
-    // articulo = new Article();
-
-    // articulo.id = 3;
-    // articulo.name = 'Galletas';
-    // articulo.imageUrl = 'https://images.unsplash.com/photo-1558961363-fa8fdf82db35?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80';
-    // articulo.price = 3.20;
-    // articulo.isOnSale = true;
-    // articulo.quantityInCart = 0;
-
-    // this.articulos.push(articulo);
-    
-
    }
 
    getArticles(query: string) : Observable<Article[]> {
